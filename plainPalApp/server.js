3af1617980b76c8709324e26c38688a81bcdc094
Achintya Ashok <aa3099@nyu.edu>
2016-09-11 22:41:42 -0400

    Changed structure of client-side app so that we can configure it to use the module organizer webpack

--- plainPalApp/server.js
+++ plainPalApp/server.js
@@ -1,20 +1,34 @@
 var express     = require('express');
-var app         = express();
 var bodyParser  = require("body-parser");
 var Promise     = require("bluebird");
+var webpack     = require('webpack');
+var config      = require('./webpack.config');
 
 // Listens on this port
 const PORT      = 8080;
+
+var app = express();
+var compiler = webpack(config);
+// all the javascripts get packaged intoa  single app.js (as configured in the webpack config file)
+// this tells the app to intercept all request and use the compiled app.js module
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 // set up the app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // convert incoming json payloads into objects
 
+// Send static files. Anything under this gets served up statically
+app.use(express.static("build"));
+
 // Set up the router to intercept requests to routes
 var router = express.Router();
 
 router.get("/", function(request, response){
-  response.status(200).json({ message: "Welcome to PlainPal" });
+  response.sendFile(path.join(__dirname, "build/index.html"));
+  // response.status(200).json({ message: "Welcome to PlainPal" });
 });
 
 app.use('/', router);
