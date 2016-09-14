99cae90f1c628eaf79e467efe1429c9de9e17a4b
Achintya Ashok <aa3099@nyu.edu>
2016-09-13 20:40:09 -0400

    adding dependencies for react

--- plainPalApp/server.js
+++ plainPalApp/server.js
@@ -34,20 +34,3 @@ router.get("/", function(request, response){
 app.use('/', router);
 app.listen(PORT);
 console.log("Started PlainPalApp\nListening on... ", PORT);
-
-
-
-// // app.use(stormpath.init(app, {
-// //   web: {
-// //     produces: ['application/json']
-// //   }
-// // }));
-//
-// app.on('stormpath.ready', function () {
-//   app.listen(3000, 'localhost', function (err) {
-//     if (err) {
-//       return console.error(err);
-//     }
-//     console.log('Listening at http://localhost:3000');
-//   });
-// });
